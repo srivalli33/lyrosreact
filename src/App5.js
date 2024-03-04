8b173c64fc1e74692796f8262a680e077441b9ba
praveen <nairxprv@gmail.com>
2024-03-04 20:47:14 +0530

    Added Images

--- src/App5.js
+++ src/App5.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 export default function App5() {
-//   let names = ["John", "Cathy", "Mike"];
   let products = [
-    {id:1,name:"Product 1",price:440},
-    {id:2,name:"Product 2",price:560},
-    {id:3,name:"Product 3",price:500},
-    {id:4,name:"Product 4",price:100},
-    {id:5,name:"Product 5",price:900},
-  ]
+    { id: 1, name: "Product 1", price: 440, image: "/lyrosreact/images/1.PNG" },
+    { id: 2, name: "Product 2", price: 560, image: "/lyrosreact/images/2.png" },
+    { id: 3, name: "Product 3", price: 500, image: "/lyrosreact/images/3.png" },
+    { id: 4, name: "Product 4", price: 100, image: "/lyrosreact/images/4.png" },
+    { id: 5, name: "Product 5", price: 900, image: "/lyrosreact/images/5.png" },
+    { id: 6, name: "Product 6", price: 900, image: "/lyrosreact/images/6.png" },
+  ];
   return (
-    <ul>
-      {products.map((e,i) => (
-        <li key={i}>{e.name}-{e.price}</li>
+    <div>
+      {products.map((e) => (
+        <div key={e.id}><img src={e.image}/></div>
       ))}
-    </ul>
+    </div>
   );
 }
