31a2a8e91d69859c48c737ca20b54e39e524f92e
praveen <nairxprv@gmail.com>
2024-03-04 21:02:54 +0530

    Changed image to uppercase

--- src/App5.js
+++ src/App5.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function App5() {
   let products = [
     { id: 1, name: "Product 1", price: 440, image: "/lyrosreact/images/1.PNG" },
-    { id: 2, name: "Product 2", price: 560, image: "/lyrosreact/images/2.png" },
-    { id: 3, name: "Product 3", price: 500, image: "/lyrosreact/images/3.png" },
-    { id: 4, name: "Product 4", price: 100, image: "/lyrosreact/images/4.png" },
-    { id: 5, name: "Product 5", price: 900, image: "/lyrosreact/images/5.png" },
-    { id: 6, name: "Product 6", price: 900, image: "/lyrosreact/images/6.png" },
+    { id: 2, name: "Product 2", price: 560, image: "/lyrosreact/images/2.PNG" },
+    { id: 3, name: "Product 3", price: 500, image: "/lyrosreact/images/3.PNG" },
+    { id: 4, name: "Product 4", price: 100, image: "/lyrosreact/images/4.PNG" },
+    { id: 5, name: "Product 5", price: 900, image: "/lyrosreact/images/5.PNG" },
+    { id: 6, name: "Product 6", price: 900, image: "/lyrosreact/images/6.PNG" },
   ];
   return (
     <div>
