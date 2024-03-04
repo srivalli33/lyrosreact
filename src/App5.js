fc5e558be55c3d3267699ae3ea6ddbeaebc2b3a3
praveen <nairxprv@gmail.com>
2024-03-04 15:19:22 +0530

    Changed App5

--- src/App5.js
+++ src/App5.js
@@ -5,8 +5,9 @@ export default function App5() {
   let products = [
     {id:1,name:"Product 1",price:440},
     {id:2,name:"Product 2",price:560},
-    {id:2,name:"Product 3",price:500},
-    {id:2,name:"Product 4",price:100},
+    {id:3,name:"Product 3",price:500},
+    {id:4,name:"Product 4",price:100},
+    {id:5,name:"Product 5",price:900},
   ]
   return (
     <ul>
