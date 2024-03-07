6b3ef19ac085fbe9ff0ec36155c530c5a928f4c1
praveen <nairxprv@gmail.com>
2024-03-07 09:48:04 +0530

    memo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App5 from "./App5";
+import App12 from "./App12";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <App5   />
+    <App12   />
   // </React.StrictMode>
 );
