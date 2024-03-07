bcf5138c8f4923bee1ad34f67d2e7c2522e41d06
praveen <nairxprv@gmail.com>
2024-03-07 09:44:37 +0530

    testing images

--- src/App6.js
+++ src/App6.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-export default function App6() {
-  const [runs, setRuns] = useState(0);
-  const [wickets, setWickets] = useState(0);
-  console.log("Component loaded");
-  useEffect(() => {
-    console.log("Better Luck Next Time!");
-  },[wickets]);
-  return (
-    <>
-      <button onClick={() => setRuns((prevState) => prevState + 1)}>
-        Runs({runs})
-      </button>
-      <button onClick={() => setWickets((prevState) => prevState + 1)}>
-        Wickets({wickets})
-      </button>
-    </>
-  );
-}
+// export default function App6() {
+//   const [runs, setRuns] = useState(0);
+//   const [wickets, setWickets] = useState(0);
+//   console.log("Component loaded");
+//   useEffect(() => {
+//     console.log("Better Luck Next Time!");
+//   },[wickets]);
+//   return (
+//     <>
+//       <button onClick={() => setRuns((prevState) => prevState + 1)}>
+//         Runs({runs})
+//       </button>
+//       <button onClick={() => setWickets((prevState) => prevState + 1)}>
+//         Wickets({wickets})
+//       </button>
+//     </>
+//   );
+// }
 
 
 ///////////////
@@ -37,14 +37,14 @@ export default function App6() {
 // }
 
 ///////////////
-// export default function App6() {
-//   const [count, setCount] = useState(0);
-//   console.log("Component loaded");
-//   return (
-//     <>
-//       <button onClick={() => setCount((prevState) => 5)}>Click</button>
-//       <span>{count}</span>
-//     </>
-//   );
-// }
+export default function App6() {
+  const [count, setCount] = useState(0);
+  console.log("Component loaded");
+  return (
+    <>
+      <button onClick={() => setCount((prevState) => 5)}>Click</button>
+      <span>{count}</span>
+    </>
+  );
+}
 ///////////////
--- src/App9.js
+++ src/App9.js
@@ -1,31 +1,25 @@
 import React from "react";
-import { useMemo, useState, useEffect } from "react";
+import { useMemo, useState } from "react";
 export default function App9() {
-  const [data, setData] = useState(0);
-  const [user, setUser] = useState(0);
-  
-  const [result, setResult] = useState();
-  setResult(a + b);
-  const getA = () => {
-    console.log("getA is called");
-    setA(Math.round(Math.random() * 10))
-  };
-  const calc = () => {
-    return a + b;
-  };
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(10);
 
-  const v1 = useMemo(() => getA(), [a]);
-  const v2 = useMemo(() => calc(), [b]);
+  const f1 = () => {
+    let i;
+    for (i = 0; i < flag ** 2; i++) {}
+    console.log("result function called");
+    return i;
+  };
+    const result = useMemo(() => f1(), [flag]);
+  // const result = f1();
 
   return (
     <div>
       <h1>useMemo</h1>
-      <hr></hr>
-      A:{v1}
-      B:{b}
-      Result:{v1 + b}
-      <button onClick={() => calc("a is updated")}>Change A</button>
-      <button onClick={() => setB(Math.round(Math.random() * 10))}>Calc</button>
+      <button onClick={() => setCount(count + 1)}>Count:{count}</button>
+      <button onClick={() => setFlag(flag + 1)}>Flag:{flag}</button>
+      <p>Result:{result}</p>
     </div>
   );
 }
+
