662c45840d82d79b30b04af88dc62f5505b0ad9b
praveen <nairxprv@gmail.com>
2024-03-09 16:22:56 +0530

    Component Life Cycle Updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App15 from "./App15";
+import App16 from "./App16";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App15 />
+    <App16 />
   </>
 );
