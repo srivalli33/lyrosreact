41e94c8127b35ce2ce53bf081ac0888a8ba00378
praveen <nairxprv@gmail.com>
2024-03-11 15:10:50 +0530

    App13a has been added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App18 from "./App18";
+import App13a from "./App13a";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App18 />
+    <App13a />
   </>
 );
