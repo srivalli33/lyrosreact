46f259d3fb76c9ccdfef44f2bf370450708d7a36
praveen <nairxprv@gmail.com>
2024-03-12 17:01:31 +0530

    Added useReducer hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App20 from "./App20";
+import App21 from "./App21";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App20 />
+    <App21 />
   </>
 );
